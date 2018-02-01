7653521596284213ef0013b26e9d5bd79711654c
Liam Hawkins <liamhawkinscu@gmail.com>
2018-02-01 12:09:42 -0500

    Files download as single zip

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 if (window.File && window.FileReader && window.FileList && window.Blob) {
+	var zip = new JSZip();
+	var numRemaining = 0;
+
+    document.getElementById('submit-button').addEventListener("click", uploadFiles);
 
     function isValidRead(read) {
         return read[0][0] == "@" && read[2][0] == "+" && read[1].length === read[3].length
@@ -6,6 +10,7 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 
     function convert(fastq_reads) {
         var fasta_reads = [];
+
         for (var i = 0; i < fastq_reads.length; i++) {
             fastq_reads[i][0] = ">" + fastq_reads[i][0].substring(1);
             fastq_reads[i].splice(2,2)
@@ -17,50 +22,30 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
 		return reads.map(e => e.join('\n')).join('\n\n');
 	};
 
-    function download(file_name, reads) {
-        var file_name_extension = file_name + ".fa";
-        var content = readsToString(reads);
-		console.log(content);
-
-        var pom = document.createElement('a');
-		pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
-		pom.setAttribute('download', file_name_extension);
-
-		if (document.createEvent) {
-			var event = document.createEvent('MouseEvents');
-			event.initEvent('click', true, true);
-			pom.dispatchEvent(event);
-		}
-		else {
-			pom.click();
-		}
-    };
-
-    function parse_file(file) {
-    // TODO: Validate FASTQ format
-    // TODO: Write to zip
-
-
+    function parseFile(file) {
         // Check if fastq/fq extension
         var file_ex = file.name.split('.')[1];
+        var reader = new FileReader();
+
         if(file_ex != 'fastq' && file_ex != 'fq'){
            document.getElementById('error').innerHTML = file.name + " is not a fastq file" ;
            return true;
         };
 
-        var reader = new FileReader();
+		// When file has been loaded process it
         reader.onload = function(progressEvent){
-
+			// Split file into array of lines
             var file_array = this.result.split('\n');
+			var fastq_reads = [];
+			var fasta_reads = [];
+			var i = 0;
 
             // remove blank lines
             file_array = file_array.filter(function(elem){
                 return elem !== "";
             });
-
-            var fastq_reads = [];
-
-            for (var i=0; i<file_array.length; i+=4) {
+			// Create array containing verified FASTQ reads (4 lines)
+            for (i=0; i<file_array.length; i+=4) {
                 if (isValidRead(file_array.slice(i,i+4))){
                     fastq_reads.push(file_array.slice(i,i+4))
                 }else{
@@ -68,30 +53,34 @@ if (window.File && window.FileReader && window.FileList && window.Blob) {
                     console.log("INVALID READ")
                 }
             };
-            console.log("FASTQ reads:");
-            console.log(fastq_reads);
-
-            var fasta_reads = convert(fastq_reads);
-            console.log("FASTA reads:");
-            console.log(fasta_reads);
-
-            download(file.name.split('.')[0], fasta_reads);
+			// Convert verified FASTQ reads to FASTA reads
+            fasta_reads = convert(fastq_reads);
+			// Add FASTA files to zip file
+            zip.file(file.name.split('.')[0] + '.fa', readsToString(fasta_reads));
+            numRemaining--;
+			// When all FASTA files have been created and zipped, download zip
+            if (numRemaining == 0) {
+                zip.generateAsync({type:"blob"})
+				.then(function (blob) {
+					saveAs(blob, "fasta_files.zip");
+				});
+            }
         };
 
         reader.readAsText(file)
-
     };
 
-    function upload() {
-
+    function uploadFiles() {
         var files = document.getElementById("files").files; // FileList object
+		var i=0;
+		var file;
 
-        for (var i=0, f; f=files[i]; i++){
-            parse_file(f);
+        numRemaining = files.length;
+        for (i=0; file=files[i]; i++){
+            parseFile(file);
         };
     };
 
-    document.getElementById('submit-button').addEventListener("click", upload);
 
 } else {
 	alert('The File APIs are not fully supported in this browser.');
